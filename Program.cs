7fb8490f9c76edc86ebe7422804b4b3fa1e8e422
Gabriel Monteiro Duarte <gabrielduarte@rede.ulbra.br>
2022-03-18 18:03:31 -0300

    Criado os arquivos

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace github_course_clone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
